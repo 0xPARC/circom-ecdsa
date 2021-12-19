90346232c6cdf0c21ab909d4e7c502c28358946d
briangu33 <briangu33@gmail.com>
2021-12-18 17:20:00 -0800

    add typescript mocha chai

---
+++ test/ecdsa.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe("Sample test set", function () {
+    it('should run a test properly', function () {
+        const myNumber = 12;
+        expect(myNumber).to.equal(12);
+    })
+});
