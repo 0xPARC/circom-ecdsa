e48cd92e4f7b7a2b44b241ca026485ceb52fb28b
briangu33 <briangu33@gmail.com>
2021-12-23 02:14:54 -0800

    eth private key to eth address circuit

--- build_groupsig.sh
+++ build_groupsig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 PHASE1=circuits/pot22_final.ptau
-BUILD_DIR=build
-CIRCUIT_NAME=build_ecdsa
+BUILD_DIR=build/groupsig
+CIRCUIT_NAME=groupsig
 
 if [ -f "$PHASE1" ]; then
     echo "Found Phase 1 ptau file"
@@ -13,7 +13,7 @@ fi
 
 if [ ! -d "$BUILD_DIR" ]; then
     echo "No build directory found. Creating build directory..."
-    mkdir "$BUILD_DIR"
+    mkdir -p "$BUILD_DIR"
 fi
 
 echo "****COMPILING CIRCUIT****"
@@ -24,7 +24,7 @@ echo "DONE ($((end-start))s)"
 
 echo "****GENERATING WITNESS FOR SAMPLE INPUT****"
 start=`date +%s`
-node "$BUILD_DIR"/"$CIRCUIT_NAME"_js/generate_witness.js "$BUILD_DIR"/"$CIRCUIT_NAME"_js/"$CIRCUIT_NAME".wasm test/input.json "$BUILD_DIR"/witness.wtns
+node "$BUILD_DIR"/"$CIRCUIT_NAME"_js/generate_witness.js "$BUILD_DIR"/"$CIRCUIT_NAME"_js/"$CIRCUIT_NAME".wasm test/input_groupsig.json "$BUILD_DIR"/witness.wtns
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
