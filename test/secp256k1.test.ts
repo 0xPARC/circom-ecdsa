47c356c0a3bc6cdb50b28163704c5494ba0c6f06
Wen-Ding Li <wl678@cornell.edu>
2022-03-23 06:39:30 +0000

    add test for ecc point scalar multiplication

--- test/secp256k1.test.ts
+++ test/secp256k1.test.ts
@@ -147,6 +147,70 @@ describe("Secp256k1Double", function () {
     test_cases.forEach(test_secp256k1_double_instance);
 });
 
+describe("Secp256k1ScalarMult", function () {
+    this.timeout(1000 * 1000);
+
+    // runs circom compilation
+    let circuit: any;
+    before(async function () {
+        circuit = await wasm_tester(path.join(__dirname, "circuits", "test_secp256k1_scalarmult.circom"));
+    });
+
+    // pubx, puby, scalarx, scalary
+    var test_cases: Array<[bigint, bigint, bigint, bigint, bigint]> = [];
+
+    // 4 randomly chosen private keys
+    var privkeys: Array<bigint> = [88549154299169935420064281163296845505587953610183896504176354567359434168161n,
+                                   37706893564732085918706190942542566344879680306879183356840008504374628845468n,
+                                   90388020393783788847120091912026443124559466591761394939671630294477859800601n,
+                                   110977009687373213104962226057480551605828725303063265716157300460694423838923n];
+    var scalars: Array<bigint> = [49162213912846590284921918007854216316029616890568331838502149777137252900653n,
+        76140377069448461768020097386275705201902774674727812489842341759686817688095n,
+        110598305201199016801761605786356726057447763277828986929716844671829352701135n,
+        89529513800538853223820080909500512684436497357931942181483678921439822888771n
+    ];
+    var pubkeys: Array<Point> = [];
+    for (var idx = 0; idx < 4; idx++) {
+        var pubkey: Point = Point.fromPrivateKey(privkeys[idx]);
+        pubkeys.push(pubkey);
+    }
+
+    for (var idx = 0; idx < 4; idx++) {
+        var scalar: bigint = scalars[idx];
+        var scalar_point: Point = pubkeys[idx].multiply(scalar);
+        test_cases.push([scalar, pubkeys[idx].x, pubkeys[idx].y,
+                         scalar_point.x, scalar_point.y]);
+    }
+
+    var test_secp256k1_scalar_instance = function (test_case: [bigint, bigint, bigint, bigint, bigint]) {
+        let scalar = test_case[0];
+        let pubx = test_case[1];
+        let puby = test_case[2];
+        let scalarx = test_case[3];
+        let scalary = test_case[4];
+
+        var scalar_array: bigint[] = bigint_to_array(64, 4, scalar);
+        var pubx_array: bigint[] = bigint_to_array(64, 4, pubx);
+        var puby_array: bigint[] = bigint_to_array(64, 4, puby);
+        var scalarx_array: bigint[] = bigint_to_array(64, 4, scalarx);
+        var scalary_array: bigint[] = bigint_to_array(64, 4, scalary);
+
+        it('Testing scalar: ' + scalar + ' pubx: ' + pubx + ' puby: ' + puby + ' scalarx: ' + scalarx + ' scalary: ' + scalary, async function() { let witness = await circuit.calculateWitness({"scalar":scalar_array, "point": [pubx_array, puby_array]});
+            expect(witness[1]).to.equal(scalarx_array[0]);
+            expect(witness[2]).to.equal(scalarx_array[1]);
+            expect(witness[3]).to.equal(scalarx_array[2]);
+            expect(witness[4]).to.equal(scalarx_array[3]); 
+            expect(witness[5]).to.equal(scalary_array[0]);
+            expect(witness[6]).to.equal(scalary_array[1]);
+            expect(witness[7]).to.equal(scalary_array[2]);
+            expect(witness[8]).to.equal(scalary_array[3]);
+            await circuit.checkConstraints(witness);
+        });
+    }
+
+    test_cases.forEach(test_secp256k1_scalar_instance);
+});
+
 // TODO: figure out some way to test that if point is not on curve, pf gen should fail
 describe("Secp256k1PointOnCurve", function () {
     this.timeout(1000 * 1000);
