be4c7b38c37bcf586ff6b1a8f23c32770e6a1962
Koh Wei Jie <contact@kohweijie.com>
2022-09-22 11:39:17 -0700

    refactor bigint utility functions to test/utils.ts

--- test/secp256k1.test.ts
+++ test/secp256k1.test.ts
@@ -9,21 +9,7 @@ const F1Field = require("ffjavascript").F1Field;
 const Scalar = require("ffjavascript").Scalar;
 exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
 const Fr = new F1Field(exports.p);
-
-function bigint_to_array(n: number, k: number, x: bigint) {
-    let mod: bigint = 1n;
-    for (var idx = 0; idx < n; idx++) {
-        mod = mod * 2n;
-    }
-
-    let ret: bigint[] = [];
-    var x_temp: bigint = x;
-    for (var idx = 0; idx < k; idx++) {
-        ret.push(x_temp % mod);
-        x_temp = x_temp / mod;
-    }
-    return ret;
-}
+import { bigint_to_array } from './utils'
 
 describe("Secp256k1AddUnequal", function () {
     this.timeout(1000 * 1000);
